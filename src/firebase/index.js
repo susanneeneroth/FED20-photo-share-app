a8d09a42780c983382af35ca2614acf337849ccc
susanneeneroth <susanne@centimedia.se>
2022-02-21 21:24:02 +0100

    Added netlify file

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,13 +4,22 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
+// }
 
 // init firebase
 const app = initializeApp(firebaseConfig)
@@ -24,9 +33,4 @@ const db = getFirestore(app)
 // get firebase storage instance
 const storage = getStorage(app)
 
-export {
-	app as default,
-	auth,
-	db,
-	storage,
-}
+export { app as default, auth, db, storage }
